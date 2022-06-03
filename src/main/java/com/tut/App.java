f3b155618b73ff5445b99238364b4f90f05ebb77
shankar87dayal <shankar.87.dayal@gmail.com>
2022-06-03 14:31:36 +0530

    Creating MAVEN Project for hibernate and Adding dependencies and Configuring Hibernate in our project with hibernate.cfg.xml

---
+++ src/main/java/com/tut/App.java
@@ -0,0 +1,22 @@
+package com.tut;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "project started........." );
+        
+        
+        SessionFactory factory =new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
+    }
+}
