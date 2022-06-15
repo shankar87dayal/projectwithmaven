79b9827c7a0dfa40b35fb3439609a22b25b4af13
shankar87dayal <shankar.87.dayal@gmail.com>
2022-06-15 11:58:41 +0530

    First Hibernate Program using Annotations and save data using hibernate

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -1,6 +1,8 @@
 package com.tut;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -13,10 +15,37 @@ public class App
     {
         System.out.println( "project started........." );
         
+        //in one line 
+       // SessionFactory factory =new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
         
-        SessionFactory factory =new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+       //in multiple line
+        Configuration    cfg =new Configuration();
+        		cfg.configure("hibernate.cfg.xml");
+        		SessionFactory factory =	cfg.buildSessionFactory();
         
-        System.out.println(factory);
-        System.out.println(factory.isClosed());
+        		//creating student
+        		
+        		Student st = new Student();
+        		st.setId(101);
+        		st.setName("rohan");
+        		st.setCity("patna");
+        		System.out.println(st);
+        		
+        		
+        		Session session = factory.openSession();
+        		
+        		Transaction tx = session.beginTransaction();
+        		
+        		session.save(st);
+        		
+        		tx.commit();
+        		
+        		session.close();
+        		
+        	
+        		
+        		
+        		
+        		
     }
 }
